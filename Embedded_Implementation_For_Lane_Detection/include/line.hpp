e991b4b69b3722901cb0e50fa4320516750a3559
kky-fury <f2012524@goa.bits-pilani.ac.in>
2017-04-18 17:11:31 +0530

    Updated Header

--- Embedded_Implementation_For_Lane_Detection/include/line.hpp
+++ Embedded_Implementation_For_Lane_Detection/include/line.hpp
@@ -10,7 +10,11 @@
 #include<math.h>
 #include<cstring>
 #include<string>
+#include<algorithm>
+#include <numeric>
+#include <functional> 
 
+#include"hough.hpp"
 
 using namespace std;
 using namespace cv;
@@ -22,6 +26,13 @@ typedef struct point
 
 }Linepoint;
 
+typedef struct line_coord_t
+{
+	Linepoint startpoint;
+	Linepoint endpoint;
+
+}line_coord;
+
 class Line
 {
 
@@ -32,15 +43,14 @@ class Line
 
 		Linepoint getstartpoint();
 		Linepoint getendpoint();
+		void setPoints(line_coord* coordinates);
+};
 
-		Line(Linepoint startpoint, Linepoint endpoint);
-
-
-}
-
-
-
-
-
+void checklanewidth(vector<Line>& line_objects, int line_count);
+void getLineObjects(vector<Line>& line_objects, lin_votes* hough_lines, int image_width, int image_height);
+line_coord* getLineEndPoints(float rho, float theta_line, int image_width, int image_height);
+bool isPointInside(Linepoint point, int image_width, int image_height);
+void initializePoints(vector<Line>& line_objects, unsigned int* clist, int count);
+void initializeLinePoints(vector<Linepoint>& x_y_points, vector<Line>& line_objects);
 
 #endif
