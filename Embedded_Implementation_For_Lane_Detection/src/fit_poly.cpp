6064443ea2868d44f0284784f381ec1bb5246a37
kky-fury <f2012524@goa.bits-pilani.ac.in>
2017-05-16 17:03:46 +0530

    Added Code for Plotting Lane fitted with a second degree polynomial curve

--- Embedded_Implementation_For_Lane_Detection/src/fit_poly.cpp
+++ Embedded_Implementation_For_Lane_Detection/src/fit_poly.cpp
@@ -1,6 +1,7 @@
 #include"line.hpp"
 #include"poly.hpp"
 
+bool debug_poly = false;
 
 
 void print_matrix(const boost::numeric::ublas::matrix<double> &m)
@@ -31,22 +32,59 @@ void print_d_vector(const std::vector<double>& arr)
 
 }
 
-void getPolyFit(vector<Line>& line_objects, Mat& gray_IPM_image)
+void getPolyFit(Line& line_objects, Mat& gray_IPM_image, vector<Linepoint>& x_y_points)
 {
+			
+	vector<Linepoint> fitting_points = getPoints(x_y_points, line_objects);
+	if(fitting_points.size() < 100)
+		return;
+	vector<double> coeff = polyfit(fitting_points, 2);	
+	polyval(coeff, gray_IPM_image, line_objects);		
 
-	for(int i  =3;i<line_objects.size();i++)
+
+}
+
+vector<Linepoint> getPoints(const vector<Linepoint>& x_y_points, Line& line_obj)
+{
+	int x_limit_max = max(line_obj.startpoint.x, line_obj.endpoint.x);
+	int x_limit_min = min(line_obj.startpoint.x, line_obj.endpoint.x);
+
+	vector<Linepoint> fitting_points;
+
+	int size = (x_limit_max + 7) - (x_limit_min - 1) + 1;
+	
+	vector<int> search_points(size);
+	int j;
+
+	for(int k = x_limit_min - 1, j=0;k<=x_limit_max + 7;k++,j++)
+	{	
+		search_points[j] = k;
+	}
+	if(debug_poly)
 	{
-
-		cout<<"Size \t"<<line_objects.size()<<endl;
-		//cout<<"Startpoint \t"<<line_objects[i].startpoint.x<<"\t"<<line_objects[i].startpoint.y<<endl;
-		//cout<<"Endpoint \t"<<line_objects[i].endpoint.x<<"\t"<<line_objects[i].endpoint.y<<endl;
-		vector<double> x_points = getLinePoints(line_objects[i]);
-		vector<double> coeff = polyfit(line_objects[i].x_y_points, 2);	
-		polyval(coeff, x_points, gray_IPM_image);		
+		cout<<"Size \t"<<size<<endl;
+		print_int_vector(search_points);
+	}
+	vector<Linepoint>::const_iterator it;
+	for(it = x_y_points.begin();it<x_y_points.end();it++)
+	{
+		vector<int>::iterator iter;
+		iter = find(search_points.begin(), search_points.end(), it->x);
+		if(iter != search_points.end())
+		{
+			fitting_points.push_back({it->x,it->y});
+		}
 
 	}
 
-
+	if(debug_poly)
+	{
+		for(int i =0 ;i < fitting_points.size();i++)
+		{
+			cout<<"Coordinates \t"<<fitting_points[i].x<<"\t"<<fitting_points[i].y<<endl;	
+		}
+	}
+	return fitting_points;
 }
 
 
@@ -84,137 +122,115 @@ vector<double> polyfit(vector<Linepoint>& x_y_points, int degree)
 		}
 
 	}
-
-	//print_matrix(oXmatrix);
-	//print_matrix(oYmatrix);
+	if(debug_poly)
+	{
+		print_matrix(oXmatrix);
+		print_matrix(oYmatrix);
+	}
 
 	matrix<double> oXtMatrix(trans(oXmatrix));
-//	print_matrix(oXtMatrix);
-
+	if(debug_poly)
+	{
+		print_matrix(oXtMatrix);
+	}
 	matrix<double> oXtXMatrix(prec_prod(oXtMatrix, oXmatrix));
-//	print_matrix(oXtXMatrix);
+	if(debug_poly)
+	{
+		print_matrix(oXtXMatrix);
+	}
 
 	matrix<double> oXtYMatrix(prec_prod(oXtMatrix, oYmatrix));
-//	print_matrix(oXtYMatrix);
-
+	if(debug_poly)
+	{
+		print_matrix(oXtYMatrix);
+	}
 	permutation_matrix<int> pert(oXtXMatrix.size1());
 	const std::size_t singular = lu_factorize(oXtXMatrix, pert);
 
 	BOOST_ASSERT( singular == 0 );
 
 	lu_substitute(oXtXMatrix, pert, oXtYMatrix);
-	//print_matrix(oXtYMatrix);	
-	
+	if(debug_poly)
+	{
+		print_matrix(oXtYMatrix);	
+	}
+
 	return std::vector<double>(oXtYMatrix.data().begin(), oXtYMatrix.data().end());
 
 }
 
-void polyval(const std::vector<double>& oCoeff, const std::vector<double>& oX, Mat& gray_IPM_image)
+void polyval(const std::vector<double>& oCoeff, Mat& gray_IPM_image, Line& line_obj)
 {
-		size_t nCount =  oX.size();
-		size_t nDegree = oCoeff.size();
-		std::vector<double>	oY( nCount );
 
-		/*
-		for(int i  =0;i<oCoeff.size();i++)
-		{
-			cout<<"Coefficients value \t"<<oCoeff[i]<<endl;
-
-		}
-		*/
-
-		for(size_t i = 0; i < nCount;i++)
-		{
-			double nY = 0;
-			double nXT = 1;
-			double nX = oX[i];
-			for(size_t j = 0;j < nDegree;j++)
-			{
-				nY += oCoeff[j] * nXT;
-				nXT *= nX;	
-			}
-			//cout<<"Value of nY \t"<<nY<<endl;
-			oY[i] = nY;
-		}
-
-	/*
-	for(int i = 0;i<nCount;i++)
-	{
-		cout<<"Value of X[i] \t"<<oX[i]<<"\t is"<<oY[i]<<endl;
-	}
-	*/
-	
-	double a,b,c, x1, x2, x3, x4;
-	double determinant;
-
+	vector<Linepoint> v_unique_points;
+	double a,b,c, determinant;
+	double x1,x2;
 	a = oCoeff[2];
 	b = oCoeff[1];
-	c = oCoeff[0] - 1;
-		
-	/*value at top of the image*/
-
-	determinant = b*b - 4*a*c;
-
-	cout<<"value of determinant \t"<<determinant<<endl;
-	if(determinant > 0)
-	{
-		x1 =  (-b + sqrt(determinant)) / (2*a);
-		x2 = (-b - sqrt(determinant)) / (2*a);
-	}
-	if(determinant == 0)
-	{
-		x1 = (-b + sqrt(determinant)) / (2*a);
-	}
-
-	/*value at bottom of the image*/
-	c = oCoeff[0] - 400;
-
-	determinant = b*b - 4*a*c;
-	cout<<"value of determinant \t"<<determinant<<endl;
 	
-	if(determinant > 0)
+	for(int i = 399 ;i >= 0; i -= 1)
 	{
-		x3 =  (-b + sqrt(determinant)) / (2*a);
-		x4 = (-b - sqrt(determinant)) / (2*a);
+		c = oCoeff[0] - i;
+		determinant = b*b - 4*a*c;
+		if(determinant > 0)
+		{
+			x1 =  (-b + sqrt(determinant)) / (2*a);
+			x2 = (-b - sqrt(determinant)) / (2*a);
+		}
+		else if (determinant == 0)
+		{
+			x1 = (-b + sqrt(determinant)) / (2*a);
+		}
+	
+		double diff = fabs(x1 -x2);
+		if(diff > 8)
+		{
+				if(x1 > (line_obj.startpoint.x - 10) && x1 <  (line_obj.startpoint.x + 10))
+				{
+					v_unique_points.push_back({(int) round(x1), (int) i});
+				}
+				else
+				{
+				v_unique_points.push_back({(int) round(x2), (int) i});
+				}
+		}
+		else
+		{
+				v_unique_points.push_back({(int) round(x1), (int) i});
+				v_unique_points.push_back({(int) round(x2), (int) i});
+
+		}
 	}
-	if(determinant == 0)
+	sort(v_unique_points.begin(), v_unique_points.end(), [](const Linepoint& lhs, const Linepoint& rhs){
+			if(lhs.x != rhs.x)
+			{
+				return lhs.x  < rhs.x;
+			}
+			else
+				return lhs.y < rhs.y;
+			});
+	//cv::line(gray_IPM_image, cv::Point(94, 60), cv::Point(98,16), (0,255,0),2);
+	
+	if(debug_poly)
 	{
-		x3 = (-b + sqrt(determinant)) / (2*a);
+		for(int i =0;i<v_unique_points.size();i++)
+		{
+			cout<<"Points \t"<<v_unique_points[i].x<<"\t"<<v_unique_points[i].y<<endl;
+		}		
 	}
 
-	//cout<<"X, Y Coordinates \t"<<x1<<"\t"<<"0"<<"\t"<<x2<<"\t"<<"0"<<"\t"<<x3<<"\t"<<"400"<<"\t"<<x4<<"\t"<<"400"<<endl;
-	
-	//print_d_vector(oX);
-	//print_d_vector(oY);
-
-	set<Linepoint> unique_points;
-	
-	for(int i = 0; i < nCount ;i++)
-	{
-		unique_points.insert({(int)oX[i], (int)oY[i]});
-	}
-	
-	for(set<Linepoint>::iterator i = unique_points.begin();i != unique_points.end();i++)
-	{
-		//cout<<"Points \t"<<unique_points[i].x<<"\t"<<unique_points[i].y<<endl;
-		Linepoint elem = *i;
-		cout<<"Points \t"<<elem.x<<"\t"<<elem.y<<endl;
-
-	
-	}
-
-	/*
-	for(size_t i  = 1 ;i< nCount -1;i++)
+	for(int i = 1;i<=v_unique_points.size() - 1;i++)
 	{
 		Point pt1, pt2;
-		pt1 = {(int) oX[i], (int) oY[i] };
-		pt2 = {(int) oX[i + 1], (int) oY[i+1]};
-	//	cout<<"Point 1 \t"<<pt1.x<<"\t"<<pt1.y<<endl;
-	//	cout<<"Point 2 \t"<<pt2.x<<"\t"<<pt2.y<<endl;
-		//if(pt1.x > 0 && pt1.y > 0 && pt2.x > 0 && pt2.y > 0)
-		cv::line(gray_IPM_image, pt1, pt2, (0,255,0),2);
+		pt1 = {(int) v_unique_points[i-1].x, (int) v_unique_points[i-1].y};
+		pt2 = {(int) v_unique_points[i].x, (int) v_unique_points[i].y};
+		if(debug_poly)
+		{
+			cout<<"Point 1 \t"<<pt1.x<<"\t"<<pt1.y<<endl;
+			cout<<"Point 2 \t"<<pt2.x<<"\t"<<pt2.y<<endl;
+		}
+		cv::line(gray_IPM_image, pt1, pt2, (0,255, 0),2);
 	}
-	*/
 }
 
 vector<double> getLinePoints(Line& line_obj)
@@ -223,12 +239,13 @@ vector<double> getLinePoints(Line& line_obj)
 	Linepoint start, end;
 	start = {line_obj.startpoint.x, line_obj.startpoint.y};
 	end = {line_obj.endpoint.x, line_obj.endpoint.y};
-	
-	/*
-	cout<<"Startpoint \t"<<start.x<<"\t"<<start.y<<endl;
-	cout<<"Endpoint \t"<<end.x<<"\t"<<end.y<<endl;
-	*/
 
+	if(debug_poly)
+	{
+		cout<<"Startpoint \t"<<start.x<<"\t"<<start.y<<endl;
+		cout<<"Endpoint \t"<<end.x<<"\t"<<end.y<<endl;
+	
+	}
 	int deltay = end.y - start.y;
 	int deltax = end.x - end.y;
 
@@ -313,12 +330,13 @@ vector<double> getLinePoints(Line& line_obj)
 		
 	}
 
-	/*
-	for(int i =0;i<pixels.size() -1;i++)
+	if(debug_poly)
 	{
-		cout<<"Points \t"<<pixels[i].x<<"\t"<<pixels[i].y<<endl;
+		for(int i =0;i<pixels.size() -1;i++)
+		{
+			cout<<"Points \t"<<pixels[i].x<<"\t"<<pixels[i].y<<endl;
+		}
 	}
-	*/	
 
 	vector<double> x_values(pixels.size());
 
