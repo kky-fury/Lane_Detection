c25009b0ff3383e86ed60715937c7a3bd04b825f
kky-fury <f2012524@goa.bits-pilani.ac.in>
2017-05-31 17:05:04 +0530

    Updated Code

--- Embedded_Lane_Detection/src/Lane_Detection_main.cpp
+++ Embedded_Lane_Detection/src/Lane_Detection_main.cpp
@@ -4,18 +4,10 @@
 int main(int argc, char* argv[])
 {
 
-	//Mat input_image =  imread("/home/nvidia/Lane_Detection/Original_Images/img_2.png");
-	/*Testing Set*/
-	//Mat input_image = imread("/home/nvidia/image_2/umm_000016.png");
-
-	Mat input_image = imread("/home/nvidia/training/image_2/umm_000078.png");
+	Mat input_image = imread("/home/nvidia/training/image_2/um_000004.png");
 
 	unsigned char* h_rgb_img = input_image.data;
 
-	/*
-	Mat gray_image(IMAGE_HEIGHT_RGB, IMAGE_WIDTH_RGB, CV_8UC1);
-	unsigned char* img = gray_image.data;
-	*/
 	/*
 	
 	for(int i =0;i<IMAGE_HEIGHT_RGB;i++)
@@ -39,7 +31,6 @@ int main(int argc, char* argv[])
 
 	BirdsEyeView bev(bev_res, invalid_value,bev_xRange_minMax, bev_zRange_minMax);
 	/*Projection matrix for left color camera in rectified coordinates*/
-	//For image_0 /*um*/
 	matrix_t intrinsic_matrix
 	{
 		{7.215377000000e+02, 0.000000000000e+00, 6.095593000000e+02, 4.485728000000e+01},
@@ -47,34 +38,15 @@ int main(int argc, char* argv[])
 		{0.000000000000e+00, 0.000000000000e+00, 1.000000000000e+00, 2.745884000000e-03}
 	};
 	
-	/*For umm*/
-	/*
-	matrix_t intrinsic_matrix
-	{
-		{7.188560000000e+02, 0.000000000000e+00, 6.071928000000e+02, 4.538225000000e+01},
-		{0.000000000000e+00, 7.188560000000e+02, 1.852157000000e+02, -1.130887000000e-01},
-		{0.000000000000e+00, 0.000000000000e+00, 1.000000000000e+00, 3.779761000000e-03}
-
-	};
-	*/
 	/*Initialize Rotation Matrix (3x3) */
-	/*For um*/
 	matrix_t rotation_matrix
 	{
 		{9.999239000000e-01, 9.837760000000e-03, -7.445048000000e-03},
 		{-9.869795000000e-03, 9.999421000000e-01,-4.278459000000e-03},
 		{7.402527000000e-03, 4.351614000000e-03, 9.999631000000e-01}
 	};
-	/*For umm*/
-	/*
-	matrix_t rotation_matrix
-	{
-		{9.999454000000e-01, 7.259129000000e-03, -7.519551000000e-03},
-		{-7.292213000000e-03, 9.999638000000e-01, -4.381729000000e-03},
-		{7.487471000000e-03, 4.436324000000e-03, 9.999621000000e-01}
-	};
-	*/
-	matrix_t Tr_cam_to_road = readcalibfile("/home/nvidia/training/calib/umm_000078.txt");
+	
+	matrix_t Tr_cam_to_road = readcalibfile("/home/nvidia/training/calib/um_000004.txt");
 	//print2dvector(Tr_cam_to_road);
 
 	bev.setup(intrinsic_matrix, rotation_matrix, Tr_cam_to_road);
@@ -170,31 +142,7 @@ int main(int argc, char* argv[])
 	vector<Line> line_objects(line_count);
 	getLineObjects(line_objects, values->hough_lines,values->votes,IMAGE_WIDTH, IMAGE_HEIGHT);
 	
-	/*
-	Linepoint startpoint, endpoint;
-	for(int i =0;i<line_objects.size();i++)
-	{
-		startpoint = line_objects[i].getstartpoint();
-		endpoint = line_objects[i].getendpoint();
-	//	cout<<"StartPoint  \t"<<"X_coordinate \t"<<startpoint.x<<"\t"<<"Y_Coordinate \t"<<startpoint.y<<endl;
-	//	cout<<"EndPoint \t"<<"X_coordinate \t"<<endpoint.x<<"\t"<<"Y_Coordinate \t"<<endpoint.y<<endl;
-		cv::Point pt1, pt2;
-		pt1.x = startpoint.x;
-		pt1.y = startpoint.y;
-
-		pt2.x = endpoint.x;
-		pt2.y = endpoint.y;
-
-		line(gray_IPM_image, pt1,pt2, (255,0,0),1);
-	
-	}
-	imshow("Result", gray_IPM_image);
-	waitKey(0);
-	*/
 	vector<Linepoint> x_y_points = initializePoints(line_objects, values->clist, values->count);	
-//	getPolyFit(line_objects, gray_IPM_image, values->clist, values->count);
-//	vector<Spline> spline_objects(line_objects.size());
-//	getRansacSplines(line_objects, spline_objects, gray_IPM_image);
 	
 	/*
 	for(int i =0;i<line_objects.size();i++)
@@ -221,16 +169,6 @@ int main(int argc, char* argv[])
 
 	for(int i =0; i<line_objects.size();i++)
 	{
-		/*
-		if(fabs(line_objects[i].startpoint.x - line_objects[i].endpoint.x) > 6)
-		{
-			getPolyFit(line_objects[i], gray_IPM_image, x_y_points);	
-		}
-		else
-		{
-			fit_line(line_objects[i], gray_IPM_image);
-		}
-		*/
 		//fit_line(line_objects[i], gray_IPM_image);
 		getPolyFit(line_objects[i], gray_IPM_image, x_y_points);	
 
@@ -240,7 +178,6 @@ int main(int argc, char* argv[])
 	unsigned char* line_detected_image = gray_IPM_image.data;
 	unsigned char* perspective_image = bev.getperspectiveView(line_detected_image);
 
-	/*
 	Mat gray_IPM_image_detected(IMAGE_HEIGHT_RGB, IMAGE_WIDTH_RGB, CV_8UC1);
 	unsigned char* p_im_pointer = gray_IPM_image_detected.data;
 
@@ -254,14 +191,13 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	imshow("Result", gray_IPM_image_detected);
-	waitKey(0);
+	//imshow("Result", gray_IPM_image_detected);
+	//waitKey(0);
 	
 	//imwrite("/home/nvidia/Lane_Detected_Images_Perspective/lane_image_persp_12.png", gray_IPM_image_detected);
 
-	*/
-	auto end = std::chrono::high_resolution_clock::now();
-	cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count() << "ns" << std::endl;
+	//auto end = std::chrono::high_resolution_clock::now();
+	//cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count() << "ns" << std::endl;
 	
 	imshow("Result", gray_IPM_image);
 	waitKey(0);
